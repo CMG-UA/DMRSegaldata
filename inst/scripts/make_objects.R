7a2cd105d87ef10293eef7dd222f8b09c05cead8
vaslem <vaslemonidis@hotmail.com>
2025-10-23 00:54:07 +0200

    Added dmps lazy data

--- inst/scripts/make_objects.R
+++ inst/scripts/make_objects.R
@@ -3,3 +3,36 @@ beta <- as.matrix(read.csv("../extdata/beta.tsv.gz", row.names = 1, header = TRU
 save(beta, file = "../../data/beta.rda", compress = "xz")
 pheno <- read.csv("../extdata/pheno.tsv", row.names = 1, header = TRUE, sep = "\t")
 save(pheno, file = "../../data/pheno.rda", compress = "xz")
+if (!requireNamespace("limma", quietly = TRUE)) {
+    if (!requireNamespace("BiocManager", quietly = TRUE)) {
+        install.packages("BiocManager")
+    }
+    BiocManager::install("limma")
+}
+mvalues <- log2(beta / (1 - beta + 1e-6) + 1e-6)
+covs <- pheno[,c("Age", "Gender")]
+pheno <- pheno[, "Sample_Group"]
+pheno_and_covs <- cbind(list(x=pheno), covs)
+design <- model.matrix( ~ ., data = pheno_and_covs)
+suppressWarnings(suppressMessages(
+{ sink("/dev/null"); not.collinear <- apply(!is.na(coef(limma::lmFit(mvalues, design))),2,all); sink(); } 
+     ))
+design <- design[,not.collinear]
+
+fit <- limma::lmFit(mvalues, design)
+fit <- limma::eBayes(fit)
+
+pcols <- paste('x',unique(pheno),sep="")
+pcols <- pcols[pcols %in% colnames(design)]
+anova.res <- limma::topTable(fit, coef=pcols, number=Inf, sort.by='none')
+tab <- data.frame(
+            intercept = anova.res$AveExpr,
+            pval = anova.res$P.Value
+          )
+tab$f <- anova.res$B
+rownames(tab) <- rownames(anova.res)
+p0 <- siggenes::pi0.est(tab$pval[!is.na(tab$pval)])$p0
+tab$qval <- siggenes::qvalue.cal(tab$pval, p0)
+o <- order(tab$pval)
+dmps <- tab[o, ,drop=F]
+save(dmps, file = "../../data/dmps.rda", compress = "xz")
